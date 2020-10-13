8fe29f78af75e3b1f8426096c87dc0ae609196a1
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-10-13 11:18:26 -0700

    added role and dept questions to array - question.js

--- assets/questions.js
+++ assets/questions.js
@@ -2,32 +2,63 @@
 const questions = {
   menu: [
     {
-      type: "list",
-      name: "menu",
-      message: "What would you like to do: ",
+      type: 'list',
+      name: 'menu',
+      message: 'What would you like to do: ',
       choices: [
-        "View Depts",
-        "View Roles",
-        "View Employees",
-        "Add Dept",
-        "Add Role",
-        "Add Employee",
-        "Update Employee",
+        'View Depts',
+        'View Roles',
+        'View Employees',
+        'Add Dept',
+        'Add Role',
+        'Add Employee',
+        'Update Employee',
         'Delete Dept',
         'Delete Role',
-        'Delete Employee'
+        'Delete Employee',
       ],
     },
   ],
   viewDept: [
     {
-      type: "list",
-      name: "viewDept",
-      message: "How would you like to view the Departments: ",
-      choices: ["View without Dept Budget", "View with Dept Budget"],
+      type: 'list',
+      name: 'viewDept',
+      message: 'How would you like to view the Departments: ',
+      choices: ['View without Dept Budget', 'View with Dept Budget'],
     },
   ],
-
+  viewEmp: [
+    {
+      type: 'list',
+      name: 'viewEmp',
+      message: 'How would you like to view the employees: ',
+      choices: [
+        'view all employees',
+        'view by manager',
+        'view by role',
+        'view by dept',
+      ],
+    },
+  ],
+  addDept: [
+    {
+      type: 'input',
+      name: 'dept-name',
+      message: 'What is the name of the department: ',
+    },
+  ],
+  addRole: [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the new role: ',
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'What is the salary for the role: (commas unnecessary) ',
+    },
+  ],
 };
 
 // export for root index.js
