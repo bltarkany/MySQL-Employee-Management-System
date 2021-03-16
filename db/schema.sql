37d651da47fe49bc2fe5cc2bbc8a9ec34dbc49ab
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-03-16 11:44:36 -0700

    created employee table with role id reference

--- db/schema.sql
+++ db/schema.sql
@@ -17,8 +17,16 @@ create table role (
     title varchar(45) unique not null,
     salary decimal(12, 2) unsigned not null,
     dept_id int unsigned not null,
-    foreign key (dept_id) references department(id)
+    foreign key (dept_id) references department(id) on delete cascade
 );
 
-
+-- employee table
+create table employee (
+    id int not null auto_increment primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int not null,
+    manager_id int,
+    foreign key (role_id) references role(id) on delete cascade
+);
 
